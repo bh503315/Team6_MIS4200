138b2e37676446c3e61e613b6ee8aa0eeff3de53
Benjamin Hoffman <bh503315@ohio.edu>
2020-10-14 11:31:05 -0400

    EMPLOYEE MODEL

---
+++ Models/Employees.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Team6_MIS4200.Models
+{
+    public class Employees
+    {
+
+        [Required]
+        public Guid ID { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display (Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Primary Phone")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Office")]
+        public string Office { get; set; }
+
+        [Display(Name = "Current position")]
+        public string Position { get; set; }
+
+        [Display(Name ="Hire Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime hireDate { get; set; }
+        
+        public string photo { get; set; }
+
+    }
+}
