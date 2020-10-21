7bb16629b2fa1fbd2d509c8f0ee52783ca3d8120
Lexi Maurins <lm721516@ohio.edu>
2020-10-21 11:24:35 -0400

    dal folder

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Team6_MIS4200.Models;
+using System.Data.Entity;
+
+namespace Team6_MIS4200.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base ("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
