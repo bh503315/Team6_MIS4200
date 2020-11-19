c2974f966656f004ca90e59da892fd699ecb9739
Benjamin Hoffman <bh503315@ohio.edu>
2020-11-19 18:33:30 -0500

    DROP DOWN LIST BABAAYYYYYY

--- Models/Recognition.cs
+++ Models/Recognition.cs
@@ -12,13 +12,36 @@ namespace Team6_MIS4200.Models
        
         [Key]
         public int surveyID { get; set; }
+        [Display(Name ="Core value recognized")]
 
-        public Guid EmployeeID { get; set; }
+        public CoreValue award { get; set; }
+
+        [Display(Name = "ID of Person giving the recognition")]
+        public Guid recognizor { get; set; }
+
+        [Display(Name = "ID of Person receiving the recognition")]
+        
      
-        public string coreValue { get; set; }
+        public Guid Nominee { get; set; }
+        [Display(Name = "Date recognition given")]
 
-        public string reasoning { get; set; }
-        [ForeignKey("EmployeeID")] 
+        public DateTime recognizationDate { get; set; }
+        public enum CoreValue
+        {
+            Excellence = 1,
+            Integrity = 2,
+            Stewardship = 3,
+            Culture = 4,
+            Passion =5,
+            Innovate = 6,
+            Balanced = 7
+        }
+
+
         public virtual Employees Employees { get; set; }
+        
+        
+
+
 }
 }
